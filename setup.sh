c2e90bfc2f275cb8c795eaaac6e9ca8f28d59ed1
priunnagru <priunnagru@gmail.com>
2022-12-02 07:38:46 -0500

    Fixed bug with setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ sudo apt install -y cmake
 sudo apt install -y libsdl2-dev
 
 cd lib/enet-1.3.17
+sudo chmod +x ./configure
 ./configure
 make
 sudo make install
-sudo rm /usr/local/lib/libenet.so.7
 sudo ldconfig
 cd ../..
 
