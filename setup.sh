1078c104d1024a8780cf4d560fb4e58015911705
priunnagru <priunnagru@gmail.com>
2022-12-02 07:11:34 -0500

    Added enet library and setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt install -y build-essential
+sudo apt install -y cmake
+sudo apt install -y libsdl2-dev
+
+cd lib/enet-1.3.17
+./configure
+make
+sudo make install
+sudo rm /usr/local/lib/libenet.so.7
+sudo ldconfig
+cd ../..
+
+mkdir build
+cd build
+cmake ../
+make
